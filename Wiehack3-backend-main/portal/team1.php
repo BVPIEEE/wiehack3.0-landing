73e3990e120be4ba63342d19c613571b3728a930
harsh735 <harshsingh.170300@gmail.com>
2021-05-21 17:51:01 +0530

    updated dashboard

--- Wiehack3-backend-main/portal/team1.php
+++ Wiehack3-backend-main/portal/team1.php
@@ -41,7 +41,7 @@ echo '    </ul>
 
    <!-- External CSS -->
 <link rel="stylesheet" href="styles.css">
-<link rel="icon" href="assets/wiehacklogo3.png" />
+<link rel="icon" href="../../assets/wiehacklogo3.png" />
    <!-- Font Awesome Icons -->
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
